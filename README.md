3813009ea4445b81af638a9c80ebe6e6626642ff
ANTONIOJLO <ANTONIOJLO@ext.inditex.com>
2022-08-06 18:14:56 +0200

    Script optimized and sys args feature added

--- README.md
+++ README.md
@@ -21,23 +21,31 @@ python3 main.py
 ### Usage
 After first init the script **will check** files
 * readme.txt file -> Contain the docs of the script
-* optimized_images folder -> Contain the folder that will be use to store optimized images
-* original_images folder -> Contain the folder where user must put the images to optimize
 
-After run script it will show you the log with the actions doned
-![img.png](src/img.png)
+Usage is really simple if you just want to optimize images of current folder with default values just run 
+```
+python3 main.py
+```
 
-Then you must enter the range percent of optimization (by default is 60%)
-![img_1.png](src/img_1.png)
+And images will be optimized with 60% in current folder
 
-Then it check if there is images in the original_images folder
+Also we have params to customize the execution
 
-If you put images in the new folder and run the script 
-![img_2.png](src/img_2.png)
+***These params are***
+* img=example.jpg -> image to optimize / default * (all)
+* q=55                     -> quality to optimize / default 60 %
+* c_directory="new_folder" -> folder to search images / default "." (current directory)
+* output="new_folder"      -> folder where found images / default "." (current directory)
+* help -> show params
 
-the result with an 50% of range will be
-![img_3.png](src/img_3.png)
-![img_4.png](src/img_4.png)
+Example
+```
+python3 main.py image=example.png q=70 output=optimized_one
+```
+
+this command will create a folder named optimized_one with a copy of example.png optimized at 70%
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from PIL import Image
 import os
+import sys
+from typing import List, Tuple
 
-# import sys
+from PIL import Image
 
-FOLDERS = ('optimized_images', 'original_images')
-EXTENSIONS = [".jpg", ".jpeg", ".png", ".gift"]
-README = 'readme.txt'
-CREATED = " created"
-EXISTS = " exists"
-CAN_NOT_BE_CREATED = " can not be created"
-NOT_FOUND = " not found"
-COMPRESSED = "compress_by_infolojo_"
+FOLDERS: Tuple[str, str] = ('optimized_images', 'original_images')
+EXTENSIONS: List[str] = [".jpg", ".jpeg", ".png", ".gift"]
+
+ARGUMENT_IMAGE: str = "img"
+ARGUMENT_QUALITY: str = "q"
+ARGUMENT_C_DIRECTORY: str = "c_directory"
+ARGUMENT_OUT_PUT: str = "output"
+ARGUMENT_HELP: str = "help"
+ARGUMENTS: List[str] = [ARGUMENT_IMAGE, ARGUMENT_QUALITY, ARGUMENT_C_DIRECTORY, ARGUMENT_OUT_PUT, ARGUMENT_HELP]
+README: str = 'readme.txt'
+CREATED: str = " created"
+EXISTS: str = " exists"
+CAN_NOT_BE_CREATED: str = " can not be created"
+NOT_FOUND: str = " not found or not exist"
+COMPRESSED: str = "compress_by_infolojo_"
 
 
 class Readme:
@@ -19,8 +27,20 @@ class Readme:
         self.readme_content = """
 ImageOptimizeScript
 
-- Put in original_images file the src you want to optimize
-- In optimized_images you could find the optimized output
+Its an script with params to optimize images. 
+
+INSTALL
+pip install -r requirements.txt
+python3 main.py -> will run and optimize all the images in the current folder.
+
+USAGE use commands to run specific files
+
+Commands :
+  - img=example.jpg             -> image to optimize / default * (all)
+  - q=55                        -> quality to optimize / default 60 %
+  - c_directory="new_folder"    -> folder to search images / default "." (current directory)
+  - output="new_folder"         -> folder where found images / default "." (current directory)
+  - help                        -> show instructions
 """
 
 
@@ -29,82 +49,81 @@ class OSHelper:
         self.folders = FOLDERS
         self.readmeIn = Readme()
         self.readme_created = False
-        self.optimized_created = False
-        self.original_images_created = False
-        self.check_folders()
-        self.create_folders_and_files()
+        self.check_readme()
 
-    def check_folders(self):
+        if not self.readme_created:
+            self.create_readme()
+
+    def check_readme(self):
         """
         Scan folders and create necessary folders if is needed
         :return:
         """
-        print("Checking folders :\n")
         for item in os.scandir("."):
             if item.name == self.readmeIn.readme_name:
                 self.readme_created = True
-                print(self.readmeIn.readme_name + EXISTS)
-            if item.name == self.folders[0]:
-                self.optimized_created = True
-                print(self.folders[0] + EXISTS)
-            if item.name == self.folders[1]:
-                self.original_images_created = True
-                print(self.folders[1] + EXISTS)
 
-    def create_folders_and_files(self):
+    def create_readme(self):
+        """
+        Create a readme.txt file with instructions if this not exists
+        """
         if not self.readme_created:
             print(self.readmeIn.readme_name + NOT_FOUND)
             try:
                 file = open(self.readmeIn.readme_name, "w")
                 file.write(self.readmeIn.readme_content + os.linesep)
                 file.close()
-                print(self.readmeIn.readme_name + CREATED)
             except:
                 show_error(3, self.readmeIn.readme_name)
 
-        if not self.optimized_created:
-            print(self.folders[0] + NOT_FOUND)
-            try:
-                os.mkdir(self.folders[0])
-                print(self.folders[0] + CREATED)
-            except:
-                show_error(3, self.folders[0])
+    @staticmethod
+    def check_if_folder_exists(directory: str):
+        """
+        Check if a folder exist
+        """
+        os.path.exists(directory)
 
-        if not self.original_images_created:
-            print(self.folders[1] + NOT_FOUND)
-            try:
-                os.mkdir(self.folders[1])
-                print(self.folders[1] + CREATED)
-            except:
-                show_error(3, self.folders[1])
+    @staticmethod
+    def create_folder(directory: str):
+        """
+        Create directory
+        """
+        try:
+            os.mkdir(directory)
+        except:
+            show_error(3, directory)
 
 
 class Optimizer:
-    def __init__(self, range: int = 60):
-        self.range = range
-        self.original_images = FOLDERS[1]
-        self.optimized_folder = FOLDERS[0]
+    def __init__(self, file: str = "*", q: int = 60, current_directory: str = ".", output: str = ".",
+                 os_helper: OSHelper = OSHelper()):
+        self.file: str = file
+        self.quality: int = q
+        self.output: str = output
+        self.os_helper: OSHelper = os_helper
         self.images_to_optimize = list()
-        self.check_images()
+        self.current_folder: str = current_directory
+        self.start()
 
-    def check_images(self):
-        # Get the folder where search src
-        original_folder = "./" + self.original_images
-        optimized_folder = "./" + self.optimized_folder
+    def start(self):
+        if not self.os_helper.check_if_folder_exists(self.current_folder) and self.current_folder != ".":
+            show_error(4, self.current_folder)
 
-        print("\nChecking src in " + self.original_images + " :")
-        for image in os.listdir(original_folder):
+        # create destination directory
+        if self.output != ".":
+            self.os_helper.create_folder(self.output)
 
-            # split image between name and extension
-            name, extension = os.path.splitext(original_folder + image)
-            print(name[2:] + "-" + extension)
+        # optimize all the images
+        if self.file == "*":
+            for image in os.listdir(self.current_folder):
+                self.optimize_image(image)
+        # optimize only one image
+        else:
+            for image in os.listdir(self.current_folder):
+                if image == self.file:
+                    self.optimize_image(image)
 
-            if extension in EXTENSIONS:
-                picture = Image.open(original_folder + "/" + image)
-                picture.save(optimized_folder + "/" + COMPRESSED + str(self.range) + "%_" + image, optmize=True,
-                             quality=self.range)
-                self.images_to_optimize.append(name[2:] + extension)
-
+        # check if images has been optimized
         if len(self.images_to_optimize) == 0:
             show_error(2)
             return
@@ -112,8 +131,22 @@ class Optimizer:
         print("\n Working ...\n")
 
         for image in self.images_to_optimize:
-            print(image + "has been saved in " + self.optimized_folder + " and optimized with " + str(self.range)
+            print(image + "has been saved in " + self.output + " and optimized with " + str(self.quality)
                   + " of quality")
+
+    def optimize_image(self, image):
+        """
+        Optimize an image
+        """
+
+        # split image between name and extension
+        name, extension = os.path.splitext(self.current_folder + image)
+        if extension in EXTENSIONS:
+            # Optimize image
+            picture = Image.open(self.current_folder + "/" + image)
+            picture.save(self.output + "/" + COMPRESSED + str(self.quality) + "%_" + image, optmize=True,
+                         quality=self.quality)
+            self.images_to_optimize.append(name[2:] + extension)
 
 
 def show_error(error: int, pre: str = ""):
@@ -124,11 +157,15 @@ def show_error(error: int, pre: str = ""):
     :return:
     """
     if error == 1:
-        print("\nrange input is not valid")
+        print("\nquality input is not valid, default 60% will be applied")
     elif error == 2:
         print("\nThere is no src to optimize")
+        exit(-1)
     elif error == 3:
         print(pre + CAN_NOT_BE_CREATED)
+    elif error == 4:
+        print(pre + NOT_FOUND)
+        exit(-1)
 
 
 def run():
@@ -136,21 +173,48 @@ def run():
     Init script
     :return (unit):
     """
-    global range
-    OSHelper()
-    try:
-        range = input("\n> Input range to optimized or press enter to use default: ")
-        if range.replace(" ", "") == "":
-            range = 60
-        range = int(range)
+    os_helper = OSHelper()
+    img = "*"
+    quality: int = 60
+    current_directory = "."
+    output = "."
 
-    except:
-        show_error(1)
+    arguments = sys.argv
+    arguments.pop(0)
+    if len(arguments) >= 1:
 
-    if 0 <= range <= 100:
-        Optimizer(range)
-    else:
-        show_error(1)
+        if ARGUMENT_HELP in arguments:
+            print(os_helper.readmeIn.readme_content)
+            arguments.remove(ARGUMENT_HELP)
+            continue_flag = input("Do you want to continue with execute?\n> ").lower()
+            if "si" != continue_flag != "true":
+                exit(-1)
+
+        for argument in arguments:
+            arg, value = argument.split("=")
+            arg = arg.lower()
+            if arg == ARGUMENT_IMAGE:
+                img = value
+            elif arg == ARGUMENT_QUALITY:
+                value = int(value)
+                try:
+                    if 0 < value < 100:
+                        quality = value
+                    else:
+                        show_error(1)
+                except:
+                    show_error(1)
+            elif arg == ARGUMENT_C_DIRECTORY:
+                current_directory = value
+            elif arg == ARGUMENT_OUT_PUT:
+                output = value
+    Optimizer(
+        file=img,
+        q=quality,
+        current_directory=current_directory,
+        output=output,
+        os_helper=os_helper
+    )
 
 
 if __name__ == '__main__':
